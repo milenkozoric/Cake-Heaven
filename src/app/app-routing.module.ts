a81d626fdc3b8c7af0d9cfa437a686cf24c8c163
milenkozoric <98126743+milenkozoric@users.noreply.github.com>
2023-01-03 23:09:37 +0100

    App-routing.module upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CakeDetailComponent } from './cakes/cake-detail/cake-detail.component';
+import { CakesComponent } from './cakes/cakes.component';
+import { ContactComponent } from './core/contact/contact.component';
+import { HomeComponent } from './core/home/home.component';
+import { ProfileComponent } from './core/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"home", component: HomeComponent},
+{path:"cakes", component: CakesComponent},
+{path:"cakes/:id", component: CakeDetailComponent},
+{path:"contact", component: ContactComponent},
+{path:"profile", component: ProfileComponent},
+{path:"", redirectTo: "/home", pathMatch: "full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
