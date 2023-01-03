8bc26830e0b7cc4dfb53ea5a4f260465de525518
milenkozoric <98126743+milenkozoric@users.noreply.github.com>
2023-01-03 23:12:33 +0100

    App.module.ts upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './core/home/home.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { ProfileComponent } from './core/profile/profile.component';
+import { ContactComponent } from './core/contact/contact.component';
+import { CakesComponent } from './cakes/cakes.component';
+import {HttpClientModule} from '@angular/common/http';
+import { CakeDetailComponent } from './cakes/cake-detail/cake-detail.component'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     HomeComponent,
+    NavBarComponent,
+    ProfileComponent,
+    ContactComponent,
+    CakesComponent,
+    CakeDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
